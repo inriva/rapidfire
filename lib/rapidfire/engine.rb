39643747872b5cd83f2dc15624d46ba304756c64
Jochen Ritscher <ritscher@inriva.com>
2017-02-16 19:33:34 +0100

    add i18n config

--- lib/rapidfire/engine.rb
+++ lib/rapidfire/engine.rb
@@ -3,5 +3,8 @@ require 'active_model_serializers'
 module Rapidfire
   class Engine < ::Rails::Engine
     isolate_namespace Rapidfire
+    config.before_initialize do
+      config.i18n.load_path += Dir["#{config.root}/config/locales/**/*.yml"]
+    end
   end
 end
