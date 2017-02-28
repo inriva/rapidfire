8a3bf2721eb6cb512a8ba85b45516c6f02900cc8
Jochen Ritscher <ritscher@inriva.com>
2017-02-28 01:40:34 +0100

    fix bug

--- app/controllers/rapidfire/application_controller.rb
+++ app/controllers/rapidfire/application_controller.rb
@@ -1,7 +1,6 @@
 module Rapidfire
   class ApplicationController < ::ApplicationController
     helper_method :can_administer?
-    layout 'layouts/backend'
     def authenticate_administrator!
       unless can_administer?
         raise Rapidfire::AccessDenied.new("cannot administer questions")
--- lib/rapidfire/version.rb
+++ lib/rapidfire/version.rb
@@ -1,3 +1,3 @@
 module Rapidfire
-  VERSION = '3.0.1.6'
+  VERSION = '3.0.1.7'
 end
